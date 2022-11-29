c92ba52a0121618bb15f29352d7e4edc43fd1666
Prajwal Patil <72968227+PrajwalAI@users.noreply.github.com>
2022-11-29 11:59:39 +0530

    Update and rename README.md to Arithmetic_formatter.py

---
+++ Arithmetic_formatter.py
@@ -0,0 +1,59 @@
+# Function for the arithmetic arranger
+# Arithmetic arranger arranges the given problem into a more user friendly format also giving the output
+def arithmetic_arranger(problems, val=False):
+    arranged_problems = ''
+    if len(problems) > 5:
+        arranged_problems = "Error: Too many problems."
+        return arranged_problems
+
+    # list of all operations in str format
+    operations = list(map(lambda x: x.split()[1], problems))
+    if set(operations) != {'+', '-'} and len(set(operations)) != 2:
+        arranged_problems = "Error: Operator must be '+' or '-'."
+        return arranged_problems
+
+    numbers = []  
+    for i in problems:
+        p = i.split()
+        numbers.extend([p[0], p[2]])
+
+    if not all(map(lambda x: x.isdigit(), numbers)):
+        arranged_problems = "Error: Numbers must only contain digits."
+        return arranged_problems
+
+    if not all(map(lambda x: len(x) < 5, numbers)):
+        arranged_problems = "Error: Numbers cannot be more than four digits."
+        return arranged_problems
+      
+    # For the top row of the problem
+    top_row = ''
+    dashes = ''
+    values = list(map(lambda x: eval(x), problems))
+    solutions = ''
+    for i in range(0, len(numbers), 2):
+        space_width = max(len(numbers[i]), len(numbers[i + 1])) + 2
+        top_row += numbers[i].rjust(space_width)
+        dashes += '-' * space_width
+        solutions += str(values[i // 2]).rjust(space_width)
+        if i != len(numbers) - 2:
+            top_row += ' ' * 4
+            dashes += ' ' * 4
+            solutions += ' ' * 4
+
+    # For the bottom row of the problem
+    bottom_row = ''
+    for i in range(1, len(numbers), 2):
+        space_width = max(len(numbers[i - 1]), len(numbers[i])) + 1
+        bottom_row += operations[i // 2]
+        bottom_row += numbers[i].rjust(space_width)
+        if i != len(numbers) - 1:
+            bottom_row += ' ' * 4
+
+    if val:
+        arranged_problems = '\n'.join((top_row, bottom_row, dashes, solutions))
+    else:
+        arranged_problems = '\n'.join((top_row, bottom_row, dashes))
+    return arranged_problems
+
+# calling the arithmetic arranger function
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
